7c8aab598247451908ce1fa1858f72bd50b446b1
sergey-stack <Kasach.serzh@mail.ru>
2022-08-13 12:23:43 +0300

    New project created

---
+++ WorkWithColors/WorkWithColors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkWithColors
+//
+//  Created by сергей on 13.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
